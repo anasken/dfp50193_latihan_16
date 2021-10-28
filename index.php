3321d880433798ef3ff11b699112ce8c200ae43c
anasken <anashalim01@gmail.com>
2021-10-28 10:40:50 +0800

    latihan foreach dalam table

--- index.php
+++ index.php
@@ -26,6 +26,29 @@ $program = [
         ]
     ]
 ];
+$exercise = [
+    'Politeknik Ungku Omar' => [
+        'Sarjana Muda Teknologi Kejuteraan Awam Dengan Kepujian'=>[
+            'SPM',
+            'STPM'
+        ],
+        
+    ],
+    'Politeknik Sultan Haji Ahmad Shah' => [
+        'Diploma Kejuruteraan Awam'=>[
+            'SPM',
+            'Sijil Kolej Komuniti'
+        ],
+        'Diploma Seni Bina'=>[
+            'SPM',
+            'Sijil Kolej Komuniti'
+        ],
+        'Diploma Geomatik'=>[
+            'SPM',
+            'Sijil Kolej Komuniti'
+        ]
+    ]
+];
 ?>
 
 <!DOCTYPE html>
@@ -37,17 +60,49 @@ $program = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ulangkaji Array</title>
     <style>
-        * {
+        *{
             font-family: Verdana, Geneva, Tahoma, sans-serif;
+        }
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        th{
+            background: orange;
         }
     </style>
 </head>
-
 <body>
     <h1>Senarai Program Di Politeknik</h1>
 
+    <table>
+        <tr style="background: red; color: white">
+            <td><b>PROGRAM</b></td>
+            <td><b>KELAYAKAN</b></td>
+        </tr>
+    <?php
+    foreach ($exercise as $poli => $value){
+        echo "<th colspan=2>$poli</th>";
+        echo "<tr>";
+        foreach ($value as $course =>$test){
+            echo "<td rowspan=2>$course</td>";
+            
+            foreach($test as $kelayakan){
+                echo "<td>$kelayakan</td>";
+                echo "<tr>";
+                
+            }
+            echo '</td>';
+            echo "</tr>";
+        }
+        echo '</tr>';
+        
+    }
+    ?>
+    </table>
     <!--<pre><?php print_r($program); ?></pre>-->
-    <ul>
+    <!-- <ul>
         <?php
         foreach ($program as $poli => $value) {
 
@@ -57,7 +112,7 @@ $program = [
 
                 echo "<li>$course";
                 foreach ($test as $kelayakan) {
-                    echo "<li>$kelayakan</li>";
+                    echo "<li>$kelayakan";
                     echo '</ul>';
                     echo '</li>';
                 }
@@ -68,7 +123,7 @@ $program = [
             echo "</li>";
         }
         ?>
-    </ul>
+    </ul> -->
     <!-- <ul>
         <li>Politeknik Ungku Omar
         <ul>
